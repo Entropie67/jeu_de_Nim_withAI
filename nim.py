b2a6fe4f702f5050a7905eefacfb0812d2f99338
Entropie67 <olivier.elophe@gmail.com>
2022-11-25 09:43:08 +0100

    Plusieurs allumettes

--- nim.py
+++ nim.py
@@ -2,6 +2,7 @@ import pygame as pg
 
 pg.init()
 
+AI = []
 
 (L, H) = (900, 600)
 COULEUR_FOND = (0, 255, 255)
@@ -14,13 +15,16 @@ screen.fill(COULEUR_FOND)
 
 # load matchstick (ms)
 ms = pg.image.load('ms.png').convert_alpha()
-ms = pg.transform.scale(ms, (50, 100))
-for i in range (12):
-    screen.blit(ms, (10 + i * 20, 10))
-pos = ms.get_rect()
-print(pos)
+ms = pg.transform.scale(ms, (60, 100))
 
+allumettes = []
+for i in range(12):
+    position = (10 + i * 20, 10)
+    rec = pg.Rect((i*10, 0, 10 + i * 20, 10))
+    screen.blit(ms, rec)
+    allumettes.append(rec)
 
+print(allumettes)
 running = True
 
 while running:
