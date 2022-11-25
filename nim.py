0d373b011b55da045a82a78e3e110060030de428
Entropie67 <olivier.elophe@gmail.com>
2022-11-25 09:23:45 +0100

    Initialisation de la fenêtre

--- nim.py
+++ nim.py
@@ -2,15 +2,28 @@ import pygame as pg
 
 pg.init()
 
-(L, H) = (600, 300)
+
+(L, H) = (900, 600)
+COULEUR_FOND = (0, 255, 255)
+
 
 screen = pg.display.set_mode((L, H))
 pg.display.set_caption('Jeu de Nim (AI)')
-pg.display.update()
+screen.fill(COULEUR_FOND)
+
+
+# load matchstick (ms)
+ms = pg.image.load('ms.png').convert_alpha()
+ms = pg.transform.scale(ms, (20, 100))
+screen.blit(ms, (10, 10))
+
+
 
 running = True
 
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            running = False
+            pg.quit()
+    pg.display.update()
+    pg.display.flip()
