b4fecb94fcb4491a20cc0eb11554786e5a56f7bd
Entropie67 <olivier.elophe@gmail.com>
2022-11-25 08:32:33 +0100

    Initialisation de la fenêtre

--- nim.py
+++ nim.py
@@ -1,2 +1,16 @@
 import pygame as pg
 
+pg.init()
+
+(L, H) = (600, 300)
+
+screen = pg.display.set_mode((L, H))
+pg.display.set_caption('Jeu de Nim (AI)')
+pg.display.update()
+
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
